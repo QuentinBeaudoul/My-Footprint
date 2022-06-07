c37bfe6e98be75e33bd190756b3858e3b8558466
Quentin Beaudoul <qbeaudoul@icloud.com>
2022-06-07 16:25:57 +0200

    navigation MFFlight

---
+++ Frameworks/MFFlight/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MFFlight
+//
+//  Created by Quentin Beaudoul on 07/06/2022.
+//
+
+import Foundation
+
+final class Constants {
+    private init() {}
+
+    static let url = "https://www.carboninterface.com/api/v1/estimates"
+}
