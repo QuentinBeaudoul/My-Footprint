416f1843e78f51813c815fe234d265723af02a7d
Quentin Beaudoul <qbeaudoul@ikomobi.com>
2022-06-09 15:20:09 +0200

    Slide airports

--- Frameworks/MFFlight/SelectAirportViewController.swift
+++ Frameworks/MFFlight/SelectAirportViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import MFExtensions
 
 class SelectAirportViewController: UIViewController {
+
+    @IBOutlet weak var airportListTopConstrainte: NSLayoutConstraint!
+    @IBOutlet weak var airportListHeightConstrainte: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // Setting up gradient background color
+        if let topColor = R.color.backgroundGradientTop(), let bottomColor = R.color.backgroundGradientBottom() {
+            view.setGradientBackground(colorTop: topColor, colorBottom: bottomColor)
+        }
+
+        // Set the historyView in the bottom
+        airportListTopConstrainte.constant =
+        UIScreen.main.bounds.height -
+        UIScreen.headerHeight -
+        UIScreen.tabbarHeight -
+        100
+
+        // Set the historyView height contrainte
+        airportListTopConstrainte.constant =
+        UIScreen.main.bounds.height -
+        UIScreen.tabbarHeight -
+        UIScreen.headerHeight
     }
 
     /*
@@ -24,5 +45,23 @@ class SelectAirportViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+    @IBAction func swipeGesture(_ sender: UISwipeGestureRecognizer) {
+        switch sender.direction {
+        case.up:
+            airportListTopConstrainte.constant = 8
+        case.down:
+            airportListTopConstrainte.constant = UIScreen.main.bounds.height -
+            UIScreen.headerHeight -
+            UIScreen.tabbarHeight -
+            100
+        default:
+            break
+        }
+
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) {
+            sender.view?.superview?.layoutIfNeeded()
+        }
+    }
 
 }
