b59c974c470d87482bae8aae34553b08adbe563a
Quentin Beaudoul <qbeaudoul@ikomobi.com>
2022-06-20 18:05:29 +0200

    WIP piste d'exploration

--- Frameworks/MFNetwork/MFNetworkTests/Tests/MFNetworkTests.swift
+++ Frameworks/MFNetwork/MFNetworkTests/Tests/MFNetworkTests.swift
@@ -28,14 +28,16 @@ class MFNetworkTests: XCTestCase {
     func testGivenUrlAndParams_WhenHittingEndpoint_ThenResultIsReturned() {
         // Given
         let originalURL = URL(string: "https://app.goflightlabs.com/airports")!
+        let params = ["access_key": ApiKeyTypes.airports.rawValue]
 
         // When
         let expectedData = Bundle.decode([Airport].self, from: "Airports.json", in: Bundle(for: Self.self))
         let mockedData = try! JSONEncoder().encode(expectedData)
-        let mock = Mock(url: originalURL, dataType: .json, statusCode: 200, data: [.get: mockedData])
+        let mockedUrl = URL(string: "https://app.goflightlabs.com/airports?access_key=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiNTkwNDE2ODU4NjI4ZWY5Zjg4ZDQ3YmUxODgxNmI4NjJkMzFkNTQ1MGRjNDNkM2IyMzFlOTI2ZDk5YTMxMWNmYzE0NTJhYWYyM2RlMjcwNjgiLCJpYXQiOjE2NTQ4NTI2MTgsIm5iZiI6MTY1NDg1MjYxOCwiZXhwIjoxNjg2Mzg4NjE4LCJzdWIiOiI1OTk0Iiwic2NvcGVzIjpbXX0.ZT8wIdggtsSPq09tb7Ii3mTsAxky1GKbCV6gfmBfGiFtJo183XDX8jBjijj2ZZYXEkKXXzOI75vouY-Iu_XiDA")!
+        let mock = Mock(url: mockedUrl, dataType: .json, statusCode: 200, data: [.get: mockedData])
         mock.register()
 
-        manager?.fetchData(httpType: .GET, apiKey: .airports, url: originalURL.absoluteString, parser: [Airport].self, completion: { response in
+        manager?.fetchData(httpType: .GET, apiKey: .airports, url: originalURL.absoluteString, parameters: params, parser: [Airport].self, completion: { response in
             switch response {
                 // Then
             case .success(let result):
@@ -46,7 +48,7 @@ class MFNetworkTests: XCTestCase {
         })
 
         let expectation = expectation(description: "Two arrays should be equals")
-        wait(for: [expectation], timeout: 5.0)
+        wait(for: [expectation], timeout: 10.0)
     }
 
 }
